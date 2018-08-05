e29cd1580d8fde411836c77f49d4af87317d8287
gick <gicquel.pierreyves@gmail.com>
2018-08-05 19:08:59 +0200

    Bash script for getting the doc added

--- gmDocScripts/getIndividualDocs.sh
+++ gmDocScripts/getIndividualDocs.sh
@@ -6,7 +6,9 @@
 HTML_FILE='imagemagick.html'
 TEXT_FILE='imagemagick.txt'
 PREFIX='doc'
-DOC_NAMES=()
+DOC_REP='DOCS/'
+DOC_NAME=()
+DOC_FIRST_LINE=()
 URL='http://www.graphicsmagick.org/GraphicsMagick.html'
 i=0
 wget $URL -O $HTML_FILE
@@ -14,11 +16,14 @@ cat $HTML_FILE | html-to-text > $TEXT_FILE
 csplit -f $PREFIX $TEXT_FILE /^.*triangle_option.*/ {*}
 perl -pi -e 's/^[ \t]+|[ \t]+$//g' $PREFIX*
 DOC_FILE=$PREFIX*
-
 for f in $DOC_FILE
   do
   ((i++))
-  DOC_NAMES[$i]=$(head -1 $f | grep -o '[-].*$')
+  DOC_FIRST_LINE[$i]=$(head -1 $f | grep -o '[-].*$')
+  DOC_NAME[$i]=$(echo ${DOC_FIRST_LINE[$i]} | grep -o '[[:alpha:]]*' | head -n 1)
+  touch ${DOC_NAME[$i]}
+  echo ${DOC_FIRST_LINE[$i]} >> ${DOC_NAME[$i]}
+  tail -n +2 $f >> ${DOC_NAME[$i]}
+  mv ${DOC_NAME[$i]} $DOC_REP
+  rm $f
   done
-
-echo $DOC_NAMES
